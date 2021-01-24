790684a1cc0fc20e9d5ec367911302351efd91df
Ovsiannikov78 <seri_tanya@mail.ru>
2021-01-24 07:18:50 +0100

    24_01_2021 Getting Started with JDBC Getting rid of MySQL SSL Warning Message

--- JdbcTest/src/JdbcTest.java
+++ JdbcTest/src/JdbcTest.java
@@ -10,16 +10,16 @@ public class JdbcTest {
 		
 		try {
 			// 1. Get a connection to database
-			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "student_for_practice" , "student");
-			
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo?useSSL=false", "student_for_practice", "student");
+
 			System.out.println("Database connection successful!\n");
-			
+
 			// 2. Create a statement
 			myStmt = myConn.createStatement();
-			
+
 			// 3. Execute SQL query
 			myRs = myStmt.executeQuery("select * from employees");
-			
+
 			// 4. Process the result set
 			while (myRs.next()) {
 				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
